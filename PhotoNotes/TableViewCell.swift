28825298584af25f546e97c4d4063e2e474aae65
Ivan <godgame84@icloud.com>
2019-12-04 11:44:22 +0300

    Update .gitignore

---
+++ PhotoNotes/TableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  TableViewCell.swift
+//  PhotoNotes
+//
+//  Created by Иван Лебедев on 15.11.2019.
+//  Copyright © 2019 Иван Лебедев. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    @IBOutlet weak var cellLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
