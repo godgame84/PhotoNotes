32562530818974b16cbaf3815de4557e5d82fd9e
Ivan <godgame84@icloud.com>
2020-03-24 14:31:19 +0300

    Убарл извлечение силой из object

--- PhotoNotes/NavigationBarTitleColorFabric.swift
+++ PhotoNotes/NavigationBarTitleColorFabric.swift
@@ -8,15 +8,19 @@
 
 import Foundation
 import UIKit
+enum colorOFTitle:String {
+    case blue, green
+}
 
 class fabric {
     func colorOnTarget() -> UIColor{
     guard let object = Bundle.main.object(forInfoDictionaryKey: "NavigationBarTitleColor") else { fatalError("Unable to Find Color for NavigationBarTitle") }
-        
-        switch object as! String {
-        case "blue":
+       
+       
+        switch object {
+        case let value as String where value == colorOFTitle.blue.rawValue:
             return #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
-        case "green":
+        case let value as String where value == colorOFTitle.green.rawValue:
             return #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
         default:
             return #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
